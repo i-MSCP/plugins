03ff8f39bf8ffa5a974f16dfa002d94474cd9994
Laurent Declercq <l.declercq@nuxwin.com>
2012-10-11 20:05:46 +0200

    Added info.php file + typo

---
+++ FacebookAuth/info.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'author' => 'Laurent Declercq',
+	'email' => 'l.declercq@nuxwin.com',
+	'version' => '0.0.1',
+	'date' => '2012-10-09',
+	'name' => 'FacebookAuth',
+	'desc' => 'Plugin allowing to authenticate users with their Facebook account data',
+	'url' => 'http://wiki.i-mscp.net/doku.php?id=plugins:facebookauth',
+    'depends' => 'dev-master'
+);
