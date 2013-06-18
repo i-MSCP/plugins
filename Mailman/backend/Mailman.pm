96f4f8245164a96479a68968582f4eecf577b0ab
Laurent Declercq <l.declercq@nuxwin.com>
2013-06-18 17:54:10 +0200

    Mailman plugin: Added backend skeleton

---
+++ Mailman/backend/Mailman.pm
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+package Plugin::Mailman;
+
+use strict;
+use warnings;
+
+use iMSCP::Debug;
+use parent 'Common::SingletonClass';
+
+sub install
+{
+	0;
+}
+
+sub uninstall
+{
+	0;
+}
+
+sub process
+{
+	0;
+}
+
+sub _addList
+{
+	0;
+}
+
+sub _updateList
+{
+	0;
+}
+
+sub _deleteList
+{
+	0;
+}
+
+1;
