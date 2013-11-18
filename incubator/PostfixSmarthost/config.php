c86dd6875e95170148a31763167a134d06ec4183
Laurent Declercq <l.declercq@nuxwin.com>
2013-11-18 04:47:20 +0100

    Cleanup

--- incubator/PostfixSmarthost/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-	'relayHost' => '', # eg smpt.domain.tld
-	'relayPort' => '587', # eg 587
-	'saslAuthUser' => '',
-	'saslAuthPasswd' => '',
-	'saslPasswdMapsFile' => '/etc/postfix/imscp/sasl_passwd'
-);
