af722c774e678143cc2e4959e9255d9bd89697ef
Laurent Declercq <l.declercq@nuxwin.com>
2014-01-31 21:08:37 +0100

    Released Mailgraph plugin (version 0.0.3) Removed useless files

--- incubator/Mailgraph/README.md
+++ incubator/Mailgraph/README.md
@@ -22,7 +22,7 @@ See [GPL v2](http://www.gnu.org/licenses/gpl-2.0.html "GPL v2")
 
 ### REQUIREMENTS
 
- - i-MSCP versions >= 1.1.0-rc4.7
+ - i-MSCP versions >= 1.1.0
  - mailgraph Debian package
 
 ### INSTALLATION AND UPDATE
--- incubator/Mailgraph/info.php
+++ incubator/Mailgraph/info.php
@@ -30,7 +30,7 @@ return array(
 	'author' => 'Sascha Bay',
 	'email' => 'info@space2place.de',
 	'version' => '0.0.3',
-	'date' => '2014-01-26',
+	'date' => '2014-01-31',
 	'name' => 'Mailgraph',
 	'desc' => 'Plugin providing statistical graphics for SMTP traffic (Postfix and Sendmail).',
 	'url' => 'http://wiki.i-mscp.net/doku.php?id=plugins:mailgraph'
