34501b6947f11374f7a8baa01b3d1637af28d5b8
Laurent Declercq <l.declercq@nuxwin.com>
2014-02-07 02:58:40 +0100

    TemplateEditor plugin: In progress...

--- incubator/TemplateEditor/config.php
+++ incubator/TemplateEditor/config.php
@@ -19,37 +19,176 @@
  */
 
 return array(
-	'service_templates' => array(
+	// Service template definitions.
+	'templates' => array(
+		// Bind9 Templates
 		'bind9' => array(
-			'named.conf' => array(
-				'path' => '/etc/imscp/bind/named.conf',
-				'scope' => 'system'
+			'global' => array(
+				'named.conf' => array(
+					'path' => '/etc/imscp/bind/named.conf',
+					'scope' => 'system'
+				),
+				'named.conf.local' => array(
+					'path' => '/etc/imscp/bind/named.conf.local',
+					'scope' => 'system'
+				),
+				'named.conf.options' => array(
+					'path' => '/etc/imscp/bind/named.conf.options',
+					'scope' => 'system'
+				)
 			),
-			'named.conf.local' => array(
-				'path' => '/etc/imscp/bind/named.conf.local',
-				'scope' => 'system'
+			'master_zone' => array(
+				'cfg_master.tpl' => array(
+					'path' => '/etc/imscp/bind/parts/cfg_master.tpl',
+					'scope' => 'site'
+				),
+				'db.tpl' => array(
+					'path' => '/etc/imscp/bind/parts/db.tpl',
+					'scope' => 'site'
+				),
+				'db_sub.tpl' => array(
+					'path' => '/etc/imscp/bind/parts/db_sub.tpl',
+					'scope' => 'site'
+				)
 			),
-			'named.conf.options' => array(
-				'path' =>  '/etc/imscp/bind/named.conf.options',
-				'scope' => 'system'
-			),
-			'cfg_master.tpl' => array(
-				'path' => '/etc/imscp/bind/parts/cfg_master.tpl',
-				'scope' => 'site'
-			),
-			'cfg_slave.tpl' => array(
-				'path' => '/etc/imscp/bind/parts/cfg_slave.tpl',
-				'scope' => 'site'
-			),
-			'db.tpl' => array(
-				'path' => '/etc/imscp/bind/parts/db.tpl',
-				'scope' => 'site'
-			),
-			'db_sub.tpl' => array(
-				'path' => '/etc/imscp/bind/parts/db_sub.tpl',
-				'scope' => 'site'
+			'slave_zone' => array(
+				'cfg_slave.tpl' => array(
+					'path' => '/etc/imscp/bind/parts/cfg_slave.tpl',
+					'scope' => 'site'
+				)
 			)
-		)
-		// TODO other services
+		),
+
+		/*
+		 // Apache2 Templates (adaoter not implemented yet)
+		'apache2' => array(
+			'global' => array(
+				'00_nameserver.conf' => array(
+					'path' => '/etc/imscp/apache/00_nameserver.conf',
+					'scope' => 'system'
+				),
+			),
+			'panel' => array(
+				'00_master.conf' => array(
+					'path' => '/etc/imscp/apache/00_master.conf',
+					'scope' => 'system'
+				),
+				'00_master_ssl.conf' => array(
+					'path' => '/etc/imscp/apache/00_master_ssl.conf',
+					'scope' => 'system'
+				)
+			),
+			'vhost' => array(
+				'domain.tpl' => array(
+					'path' => '/etc/imscp/apache/parts/domain.tpl',
+					'scope' => 'site'
+				),
+				'domain_ssl.tpl' => array(
+					'path' => '/etc/imscp/apache/parts/domain_ssl.tpl',
+					'scope' => 'site'
+				),
+				'domain_disabled.tpl' => array(
+					'path' => '/etc/imscp/apache/parts/domain_disabled.tpl',
+					'scope' => 'site'
+				),
+				'domain_disabled_ssl.tpl' => array(
+					'path' => '/etc/imscp/apache/parts/domain_disabled_ssl.tpl',
+					'scope' => 'site'
+				),
+				'domain_redirect.tpl' => array(
+					'path' => '/etc/imscp/apache/parts/domain_redirect.tpl',
+					'scope' => 'site'
+				),
+				'domain_redirect_ssl.tpl' => array(
+					'path' => '/etc/imscp/apache/parts/domain_redirect_ssl.tpl',
+					'scope' => 'site'
+				)
+			)
+		),
+		*/
+
+		/*
+		// Courier Templates (adapter not implemented yet)
+		'courier' => array(
+			'global' => array(
+				'authmysqlrc' => array(
+					'path' => '/etc/imscp/courier/authmysqlrc',
+					'scope' => 'system'
+				),
+				'quota-warning' => array(
+					'path' => '/etc/imscp/courier/quota-warning',
+					'scope' => 'system'
+				)
+			)
+		),
+		*/
+
+		/*
+		 // Dovecot Templates (adapter not implemented yet)
+		'dovecot' => array(
+			'global' => array(
+				'dovecot.conf.1' => array(
+					'path' => '/etc/imscp/dovecot/dovecot.conf.1',
+					'scope' => 'system'
+				),
+				'dovecot.conf.2' => array(
+					'path' => '/etc/imscp/dovecot/dovecot.conf.2',
+					'scope' => 'system'
+				),
+				'dovecot-sql.conf' => array(
+					'path' => '/etc/imscp/dovecot/dovecot-sql.conf',
+					'scope' => 'system'
+				),
+				'quota-warning.1' => array(
+					'path' => '/etc/imscp/dovecot/quota-warning.1',
+					'scope' => 'system'
+				),
+				'quota-warning.2' => array(
+					'path' => '/etc/imscp/dovecot/quota-warning.1',
+					'scope' => 'system'
+				)
+			)
+		),
+		*/
+
+		/*
+		// Postfix Templates (adapter not implemented yet)
+		'postfix' => array(
+			'system' => array(
+				'main.cf' => array(
+					'path' => '/etc/imscp/postfix/main.cf',
+					'scope' => 'system'
+				),
+				'master.cf' => array(
+					'path' => '/etc/imscp/postfix/master.cf',
+					'scope' => 'system'
+				)
+			)
+		),
+		*/
+
+		/*
+		// Proftpd Templates (adapter not implemented yet)
+		'proftpd' => array(
+			'system' => array(
+				'proftpd.conf' => array(
+					'path' => '/etc/imscp/postfix/proftpd.conf',
+					'scope' => 'system'
+				)
+			)
+		),
+		*/
+
+		// TODO PHP templates
+
+		// Level for template assigment (only reseller level is currently supported)
+		// Note: Only site-wide templates are assignable
+		'assigmnent_level' => 'reseller',
+
+		// Whether or not default template must be synced with last available versions on update
+		'sync_templates' => true,
+
+		// Whether or not obsolete templates (including childs) must be purged on update
+		'purge_obsolete' => true
 	)
 );
