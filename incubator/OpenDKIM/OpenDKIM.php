91f5abfad6bfe031f25e779bf53173aff6e82995
Laurent Declercq <l.declercq@nuxwin.com>
2014-06-06 18:14:01 +0200

    OpenDKIM plugin: Updated required plugin API version

--- incubator/OpenDKIM/OpenDKIM.php
+++ incubator/OpenDKIM/OpenDKIM.php
@@ -386,7 +386,7 @@ class iMSCP_Plugin_OpenDKIM extends iMSCP_Plugin_Action
 	protected function checkCompat($event)
 	{
 		if ($event->getParam('pluginName') == $this->getName()) {
-			if (version_compare($event->getParam('pluginManager')->getPluginApiVersion(), '0.2.9', '<')) {
+			if (version_compare($event->getParam('pluginManager')->getPluginApiVersion(), '0.2.10', '<')) {
 				set_page_message(
 					tr('Your i-MSCP version is not compatible with this plugin. Try with a newer version.'), 'error'
 				);
