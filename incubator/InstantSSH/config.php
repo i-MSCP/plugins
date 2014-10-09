e4c9057e4094ef37dacd30390afb9411bc105a1b
Laurent Declercq <l.declercq@nuxwin.com>
2014-10-09 12:19:14 +0200

    InstantSSH plugin: Updated configuration file

--- incubator/InstantSSH/config.php
+++ incubator/InstantSSH/config.php
@@ -21,7 +21,7 @@
 require_once 'InstantSSH/Validate/SshAuthOptions.php';
 
 return array(
-	// Default SSH authentication options added for any new customer key
+	// Default SSH authentication options added for any new SSH key
 	//
 	// See man authorized_keys for list of allowed authentication options.
 	// eg. command="dump /home",no-pty,no-port-forwarding
@@ -51,35 +51,38 @@ return array(
 		\InstantSSH\Validate\SshAuthOptions::ALL
 	),
 
-	// Shell for SSH users (default: /bin/bash)
+	// Shell for SSH users (default: /bin/bash for normal shell ; /bin/ash for jailed shell)
 	//
-	// See man shells for further details. Please, do not change this value if you do not know what you are doing.
-	'shell' => '/bin/bash',
+	// See man shells for further details. Please, do not change the default values if you do not know what you are doing.
+	'shells' => array(
+		// Shell for normal ssh users
+		'normal' => '/bin/bash',
 
-	##
-	### Jailed shell configuration options
-	##
+		// Shell for jailed ssh users
+		// BusyBox built-in shell ; Don't forget to set it back to /bin/bash if you do not use BusyBox built-in shell
+		// Note: /bin/ash is a link that point to the /bin/busybox executable. This link is automatically created by the
+		// plugin. The /bin/ash shell is also automatically added in the /etc/shells file.
+		'jailed' => '/bin/ash'
+	),
 
 	// Root jail directory (default: /var/chroot/InstantSSH)
 	//
 	// Full path to the root jail directory. Be sure that the partition in which this directory is living has enough
 	// space to host the jails.
-	//
-	// WARNING: You must never change this parameter while updating the plugin to a new version.
 	'root_jail_dir' => '/var/chroot/InstantSSH',
 
 	// Shared jail (default: true)
 	//
 	// When the value is true, only one jail is created for all customers. A shared jail doesn't mean that customers
 	// will be able to read, modify or delete files of other customers. This simply mean that the jail will be shared
-	// between customers. The primary purpose of a jailed shell environment is to protect the main system. Having a jail
-	// for each customer is interesting only when you want provide a different set of commands for each of them.
+	// between customers. The primary purpose of a jailed environment is to protect the main system. Having a jail for
+	// each customer is interesting only when you want provide a different set of commands for each of them.
 	//
 	// Note: The creation of a jail per customer is currently useless because the per customer application feature is
 	// not implemented yet. This will be implemented in near future.
 	'shared_jail' => true,
 
-	// Preserved files (default: /home)
+	// Preserved files (default: USER_WEB_DIR parameter value from the imscp.conf file)
 	//
 	// The plugin won't try to remove files or directories inside jails if their path begins with one of the strings
 	// in this list.
@@ -88,337 +91,97 @@ return array(
 	//
 	// WARNING: Do not remove the default entry if you don't know what you are doing.
 	'preserve_files' => array(
-		'/var/www/virtual'
+		iMSCP_Registry::get('config')->get('USER_WEB_DIR')
 	),
 
-	# Application section (default: imscpbase)
-	#
-	# This is the list of application sections which are used to create/update the jails (see below).
-	#
-	# By default only the imscpbase application section is added, which allows to build very restricted jailed shell
-	# environments (with limited set of commands).
+	// Whether or not files from packages required by packages listed in packages options must be copied whthin the jails
+	'include_pkg_deps' => false,
+
+	// Selected application sections for jailed shell environments (default: imscpbase)
+	//
+	// This is the list of application sections which are used to create/update the jails (see below).
+	//
+	// By default only the imscpbase application section is added, which allows to build a very restricted jailed shell
+	// environments using BusyBox.
 	'app_sections' => array(
 		'imscpbase'
 	),
 
-	# Predefined application sections
-	#
-	# Below, you can find the predefined application sections. Those sections are used to create and update jails.
-	# You can select as many sections as you want by adding them into the app_sections configuration option.
-	#
-	# It's not recommended to change a section without understanding its meaning and how it is working. Once you
-	# know how the sections are defined, you can define your own sections.
-	#
-	# Application section options
-	#
-	# The following options can be defined in application sections
-	#
-	# path: List of paths which have to be copied inside the jail. Be aware that copy is not recursive.
-	# packages: List of debian packages. Files from those packages will be copied inside the jail
-	# include_pkg_deps: Whether or not files from packages required by packages listed in packages option must be copied
-	# include_apps_sections: List of applications sections that have to be included
-	# users: List of users that have to be added inside the jail (eg. in passwd/shadow files)
-	# groups: List of groups that have to be added inside the jail (eg. in group/gshadow files)
-	# preserve_files: Files that have to be preserved when the jail is being updated
-	# devices: List of devices that have to be created inside the jail
-	# mount: List of directory/file to remount in jail as key/value  pairs where a key correspond to oldir (system) and
-	#        the value to newdir (jail) (see man mount)
-	#
-	# WARNING: Some application sections are still experimental and can require few adjustements.
+	// Predefined application sections for jailed shell environments
+	//
+	// Below, you can find the predefined application sections. Those sections are used to create and update the jails.
+	// You can select as many sections as you want by adding them into the app_sections configuration option above.
+	//
+	// It's not recommended to change a section without understanding its meaning and how it is working. Once you know
+	// how the sections are defined, you can define your own sections.
+	//
+	// Application section options
+	//
+	// The following options can be defined in application sections
+	//
+	// paths: List of paths which have to be copied inside the jail. Be aware that copy is not recursive.
+	// packages: List of debian packages. Files from those packages will be copied inside the jail
+	// include_apps_sections: List of applications sections that have to be included
+	// users: List of users that have to be added inside the jail (eg. in passwd/shadow files)
+	// groups: List of groups that have to be added inside the jail (eg. in group/gshadow files)
+	// preserve_files: Files that have to be preserved when a jail is being updated
+	// devices: List of devices that have to be copied inside the jail
+	//
+	// Notes:
+	//  - All options which contain paths (paths, devices) support the glob patterns.
+	//  - Any path which doesn't exists on the system is ignored
+	//  - Any package listed in a package option must be already installed on the system, else an error is thrown
+	//  - Any device must exists on the system, else an error is thrown. You must use glob patterns to avoid any error
+	//
+	// Other application sections will be added soon.
 
 	// uidbasics section
 	// Provide common files for jails that need user/group information
 	'uidbasics' => array(
 		'paths' => array(
-			'/lib/libnsl.so.1', '/lib64/libnsl.so.1', '/lib/libnss*.so.2', '/lib64/libnss*.so.2',
-			'/lib/i386-linux-gnu/libnsl.so.1', '/lib/i386-linux-gnu/libnss*.so.2', '/lib/x86_64-linux-gnu/libnsl.so.1',
-			'/lib/x86_64-linux-gnu/libnss*.so.2', '/etc/nsswitch.conf', '/etc/ld.so.conf', '/etc/passwd', '/etc/group'
+			'/etc/ld.so.conf', '/etc/passwd', '/etc/group', '/etc/nsswitch.conf', '/lib/libnsl.so.1',
+			'/lib64/libnsl.so.1', '/lib/libnss*.so.2', '/lib64/libnss*.so.2', '/lib/i386-linux-gnu/libnsl.so.1',
+			'/lib/i386-linux-gnu/libnss*.so.2', '/lib/x86_64-linux-gnu/libnsl.so.1',
+			'/lib/x86_64-linux-gnu/libnss*.so.2'
 		)
 	),
 
-	// netbasics section
-	// Provide common files for all jails that need any internet connectivity
-	'netbasics' => array(
+	// busyboxshell section
+	// Provide restricted shell using BusyBox (built-in shell and common UNIX utilities)
+	'busyboxshell' => array(
 		'paths' => array(
-			'/lib/libnss_dns.so.2', '/lib64/libnss_dns.so.2', '/etc/resolv.conf', '/etc/host.conf', '/etc/hosts',
-			'/etc/protocols', '/etc/services'
-		)
-	),
-
-	// logbasics section
-	// Provide timezone information and log sockets
-	'logbasics' => array(
-		'paths' => '/etc/localtime',
-		'need_logsocket' => true
-	),
-
-	// basicshell section
-	// Provide bash based shell with several basic utilities
-	'basicshell' => array(
-		'paths' => array(
-			'/bin/sh', '/bin/bash', '/bin/ls', '/bin/cpio', '/bin/egrep', '/bin/fgrep', '/bin/grep', '/bin/gunzip',
-			'/bin/gzip', '/bin/more', '/usr/bin/rgrep', '/bin/sed', '/bin/tar', '/bin/uncompress', '/bin/zcat',
-			'/usr/bin/lzma', '/usr/bin/xz', '/etc/motd', '/etc/issue', '/etc/bash.bashrc', '/etc/bashrc', '/etc/profile',
-			'/usr/lib/locale/C.UTF-8'
-		),
-		'users' => array(
-			'root'
-		),
-		'groups' => array(
-			'root'
+			'/bin/ash',
+			'/proc'
 		),
 		'include_app_sections' => array(
-			'uidbasics'
-		),
-		'packages' => array(
-			'coreutils' # Package which provide basic file, shell and text manipulation utilities
-		)
-	),
-
-	// extendedshell section
-	// Provide bash shell including things like awk, bzip, tail, less
-	'extendedshell' => array(
-		'paths' => array(
-			'/usr/bin/awk', '/bin/bzip2', '/bin/bunzip2', '/usr/bin/ldd', '/usr/bin/less', '/usr/bin/clear',
-			'/usr/bin/cut', '/usr/bin/find', '/usr/bin/less', '/usr/bin/watch',
-		),
-		'include_app_sections' => array(
-			'basicshell', 'midnightcommander', 'editors', 'terminfo'
-		)
-	),
-
-	// editors section
-	// Provide vim, joe, nano and pico
-	'editors' => array(
-		'paths' => array(
-			'/usr/bin/joe', '/usr/bin/nano', '/usr/bin/vi', '/usr/bin/vim', '/usr/bin/pico', '/etc/vimrc', '/etc/joe',
-			'/usr/share/vim', '/usr/bin/pico'
-		),
-		'include_apps_sections' => array(
-			'terminfo'
-		)
-	),
-
-	// netutils section
-	// Provide several internet utilities like wget, ftp, rsync, scp, ssh
-	'netutils' => array(
-		'paths' => array( # TODO review (resolv.conf ?)
-			'/usr/bin/wget', '/usr/bin/lynx', '/usr/bin/ftp', '/usr/bin/host', '/usr/bin/smbclient', '/usr/bin/dig'
-		),
-		'include_app_sections' => array(
-			'netbasics', 'ssh', 'sftp', 'scp', 'rsync'
-		)
-	),
-
-	// section extshellplusnet
-	// alias for extendedshell + netutils + apacheutils
-	'extshellplusnet' => array(
-		'include_app_sections' => array(
-			'extendedshell', 'netutils', 'apacheutils'
-		)
-	),
-
-	// midnightcommander section
-	// Midnight Commander
-	'midnightcommander' => array(
-		'paths' => array(
-			'/usr/bin/mc', '/usr/bin/mcedit', '/usr/bin/mcview', '/usr/share/mc'
-		),
-		'include_app_sections' => array(
-			'basicshell', 'terminfo'
-		)
-	),
-
-	// scp section
-	// Provide ssh secure copy
-	'scp' => array(
-		'paths' => array(
-			'/usr/bin/scp'
-		),
-		'include_app_sections' => array(
-			'netbasics', 'uidbasics'
-		),
-		'devices' => array(
-			'/dev/urandom'
+			'uidbasics',
+			'busybox'
 		),
 		'preserve_files' => array(
 			'/dev'
-		)
-	),
-
-	// sftp section
-	// Provide ssh secure ftp
-	'sftp' => array(
-		'paths' => array(
-			'/usr/lib/sftp-server', '/usr/libexec/openssh/sftp-server', '/usr/lib/misc/sftp-server',
-			'/usr/libexec/sftp-server', '/usr/lib/openssh/sftp-server'
-		),
-		'include_app_sections' => array(
-			'netbasics', 'uidbasics'
 		),
 		'devices' => array(
-			'/dev/urandom', '/dev/null'
-		),
-		'preserve_files' => array(
-			'/dev'
-		)
-	),
-
-	// ssh section
-	// Provide ssh secure shell
-	'ssh' => array(
-		'paths' => array(
-			'/usr/bin/ssh'
-		),
-		'include_app_sections' => array(
-			'netbasics', 'uidbasics'
-		),
-		'devices' => array( # TODO review
-			'/dev/urandom', '/dev/tty', '/dev/null'
-		),
-		'preserve_files' => array(
-			'/dev'
-		)
-	),
-
-	// rsync section
-	// Provide rsync command
-	'rsync' => array(
-		'paths' => array(
-			'/usr/bin/rsync'
-		),
-		'include_app_sections' => array(
-			'netbasics', 'uidbasics'
-		)
-	),
-
-	// cvs section
-	// Provide concurrent Versions System
-	'cvs' => array(
-		'paths' => array(
-			'/usr/bin/cvs'
-		),
-		'devices' => array(
+			'/dev/ptmx',
+			'/dev/pty*',
+			'/dev/pts/*',
+			'/dev/tty*',
+			'/dev/urandom',
+			'/dev/zero',
 			'/dev/null'
-		),
-		'preserve_files' => array(
-			'/dev'
 		)
 	),
 
-	// git section
-	// Provide Git - Distributed revision control and source code management (SCM) system
-	'git' => array(
-		'paths' => array(
-			'/usr/bin/git*', '/usr/lib/git-core'
-		),
-		'commands' => array(
-			'basename', 'uname'
-		),
-		'include_app_sections' => array(
-			'editors', 'perl'
-		)
-	),
-
-	// procmail section
-	// Provide procmail mail delivery agent
-	'procmail' => array(
-		'paths' => array(
-			'/usr/bin/procmail', '/bin/sh'
-		),
-		'devices' => array(
-			'/dev/null'
-		),
-		'preserve_files' => array(
-			'/dev'
-		)
-	),
-
-	// terminfo section
-	// Provide terminfo databases, required for example for ncurses or vim
-	'terminfo' => array(
+	// busybox section
+	// Provide BusyBox which combines tiny versions of many common UNIX utilities into a single small executable
+	'busybox' => array(
 		'packages' => array(
-			'ncurses-base', # Package which provide terminfo data files to support the most common types of terminal
-			//'ncurses-term' # Package which provide all of the numerous terminal definitions not found in the ncurses-base
-		)
-	),
-
-	// apacheutils section
-	// Provide htpasswd utility
-	'apacheutils' => array(
-		'paths' => array(
-			'/usr/bin/htpasswd'
-		)
-	),
-
-	// perl section
-	// Provide the perl interpreter and libraries
-	'perl' => array(
-		#'paths' => array(
-		#	'/usr/bin/perl', '/usr/lib/perl', '/usr/lib/perl5', '/usr/share/perl', '/usr/share/perl5'
-		#)
-		'packages' => array(
-			'perl'
-		),
-		'include_pkg_deps' => true
-	),
-
-	// ping section
-	// Provide the ping command
-	'ping' => array(
-		'paths' => array(
-			'/bin/ping'
-		)
-	),
-
-	// xterm section
-	// Provide xterm
-	'xterm' => array(
-		'paths' => array(
-			'/usr/bin/X11/xterm'
-		),
-		'include_app_sections' => array(
-			'terminfo'
-		),
-		'devices' => array(
-			'/dev/pts/0', '/dev/pts/1', '/dev/pts/2', '/dev/pts/3', '/dev/pts/4',
-			'/dev/ptyb4', '/dev/ptya4',
-			'/dev/tty', '/dev/tty0', '/dev/tty4'
-		)
-	),
-
-	// mysqlclient section
-	// Provide MySQL client
-	'mysqlclient' => array(
-		'paths' => array(
-			'/usr/bin/mysql'
-		),
-		'users' => array(
-			'mysql'
-		),
-		'groups' => array(
-			'mysql'
-		),
-		'include_app_sections' => array(
-			'netbasics', 'uidbasics'
-		),
-		'mount' => array(
-			'/var/run/mysqld' => '/var/run/mysqld'
+			'busybox'
 		)
 	),
 
 	// imscpbase section
 	// Provide pre-selected path, application sections, users and groups for i-MSCP jailed shell
 	'imscpbase' => array(
-		'paths' => array(
-			'/bin/hostname',
-			'/usr/bin/clear_console',
-			'/usr/bin/id',
-			'/usr/bin/groups',
-			'/usr/bin/lesspipe',
-			'/usr/bin/tput',
-			'/usr/bin/which'
-		),
 		'users' => array(
 			'root', 'www-data'
 		),
@@ -426,7 +189,7 @@ return array(
 			'root', 'www-data'
 		),
 		'include_app_sections' => array(
-			'extshellplusnet', 'xterm'
+			'busyboxshell'
 		)
 	)
 );
