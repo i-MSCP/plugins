16942aec75c4875a46066c0a61d6e81dc9aea018
Laurent Declercq <l.declercq@nuxwin.com>
2014-12-17 23:06:45 +0100

    Cronjobs: Small fix

--- incubator/CronJobs/l10n/nl_NL.php
+++ incubator/CronJobs/l10n/nl_NL.php
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * @translator Djawi de Boer (Novy) <djawi@djawi.nl>
- * @translator & review Thom Heemstra (theemstra) <thom@heemstra.us>
+ * @translator Thom Heemstra (theemstra) <thom@heemstra.us>
  */
 
 return array(
