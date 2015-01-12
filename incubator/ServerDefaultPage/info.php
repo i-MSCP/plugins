ec216b624c50f83aab25aca0ef3d9a9f9abfaab5
Ninos Ego <me@ninosego.de>
2015-01-12 08:17:54 +0100

    ServerDefaultPage: Preparation for new release (v1.0.3)

--- incubator/ServerDefaultPage/info.php
+++ incubator/ServerDefaultPage/info.php
@@ -21,8 +21,8 @@
 return array(
 	'author' => 'Ninos Ego',
 	'email' => 'me@ninosego.de',
-	'version' => '1.0.2',
-	'date' => '2014-01-11',
+	'version' => '1.0.3',
+	'date' => '2014-01-12',
 	'name' => 'ServerDefaultPage',
 	'desc' => 'Plugin which allows to set a server default page.',
 	'url' => 'http://wiki.i-mscp.net/doku.php?id=plugins:serverdefaultpage'
