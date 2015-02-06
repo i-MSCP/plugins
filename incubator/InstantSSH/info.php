13c476b6cb8e55836bee707f0313c99cf9d4dcb8
Laurent Declercq <l.declercq@nuxwin.com>
2015-02-06 05:33:55 +0100

    InstantSSH: Preparation for new release

--- incubator/InstantSSH/info.php
+++ incubator/InstantSSH/info.php
@@ -21,8 +21,8 @@
 return array(
 	'author' => 'Laurent Declercq',
 	'email' => 'l.declercq@nuxwin.com',
-	'version' => '3.1.1',
-	'date' => '2014-01-14',
+	'version' => '3.1.2',
+	'date' => '2015-02-06',
 	'name' => 'InstantSSH',
 	'desc' => 'Plugin allowing to provide full or restricted shell access to your customers.',
 	'url' => 'http://wiki.i-mscp.net/doku.php?id=plugins:instantssh'
