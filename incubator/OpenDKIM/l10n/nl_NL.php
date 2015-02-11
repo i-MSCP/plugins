024b0deff8fb01bfe7110f79a48cadc336064a3a
Thom <thom@heemstra.us>
2015-02-11 20:01:12 +0100

    Added Dutch translation for OpenDKIM

---
+++ incubator/OpenDKIM/l10n/nl_NL.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * i-MSCP - internet Multi Server Control Panel
+ * Copyright (C) 2010-2015 by i-MSCP Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @category    iMSCP
+ * @package     iMSCP_Plugin
+ * @subpackage  OpenDKIM
+ * @copyright   2010-2015 by i-MSCP Team
+ * @copyright   Sascha Bay <info@space2place.de>
+ * @author      Sascha Bay <info@space2place.de>
+ * @link        http://www.i-mscp.net i-MSCP Home Site
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL v2
+ *
+ * @translator  Thom Heemstra (theemstra) <thom@heemstra.us>
+ */
+
+return array(
+	'OpenDKIM support scheduled for activation. This can take few seconds.' => 'OpenDKIM-ondersteuning is ingepland om ingeschakeld te worden. Dit kan een paar seconden duren.',
+	'OpenDKIM support scheduled for deactivation. This can take few seconds.' => 'OpenDKIM-ondersteuning is ingepland om uitgeschakeld te worden. Dit kan een paar seconden duren.',
+	'Generation in progress.' => 'Aan het genereren.',
+	'n/a' => 'N/B',
+	'OpenDKIM entries for customer: %s' => 'OpenDKIM vermeldingen voor deze klant: %s',
+	'Deactivate OpenDKIM' => 'Deactiveer OpenDKIM',
+	'No customer with OpenDKIM support has been found.' => 'Er zijn geen klanten gevonden waarvoor OpenDKIM is ingeschakeld.',
+	'Customers / OpenDKIM' => 'Klanten / OpenDKIM',
+	'encoding' => 'encoding',
+	'Select a customer' => 'Selecteer een klant',
+	'Activate OpenDKIM for this customer' => 'Activeer OpenDKIM voor deze klant',
+	'Domain Name' => 'Domeinnaam',
+	'OpenDKIM domain key' => 'OpenDKIM domeinkey',
+	'Status' => 'Status',
+	'Are you sure you want to deactivate OpenDKIM for this customer?' => 'Weet u zeker dat u OpenDKIM voor deze klant wilt uitschakelen?',
+	'No domain with OpenDKIM support has been found.' => 'Er zijn geen domeinen gevonden met OpenDKIM-ondersteuning.',
+	'Domain' => 'Domein',
+	'Name' => 'Naam',
+	'Unable to install: %s' => 'Kon het niet installeren: %s',
+	'Unable to update: %s' => 'Kon het niet bijwerken: %s',
+	'Unable to uninstall: %s' => 'Kon het niet deïnstalleren: %s',
+	'Your i-MSCP version is not compatible with this plugin. Try with a newer version.' => 'Uw versie van i-MSCP werkt niet met deze plugin. Probeer het met een nieuwere versie.',
+	'OpenDKIM' => 'OpenDKIM'
+);
