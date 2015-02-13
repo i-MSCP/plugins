88788cea5239d84aafede7c4b2f00beafc368f09
Thom <thom@heemstra.us>
2015-02-13 12:10:06 +0100

    Added Dutch translations

---
+++ incubator/Mailman/l10n/nl_NL.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * i-MSCP Mailman plugin
+ * Copyright (C) 2013-2015 Laurent Declercq <l.declercq@nuxwin.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @translator Djawi de Boer (hank09878) <djawi@djawi.nl>
+ * @translator Thom Heemstra (theemstra) <thom@heemstra.us>
+ */
+
+return array(
+	'List name is either reserved or not valid.' => 'Naam van lijst is reeds in gebruik of ongeldig.',
+	'Email is not valid.' => 'E-mail is ongeldig.',
+	'Passwords do not match.' => 'Wachtwoorden komen niet overeen.',
+	'This list already exist. Please, choose other name.' => 'Deze lijst bestaat reeds. Gelieve een andere naam te kiezen.',
+	'Edit' => 'Bewerken',
+	'Delete' => 'Verwijderen',
+	'N/A' => 'N/B',
+	'You do not have created any mailing list yet.' => 'U hebt nog geen mailinglists aangemaakt.',
+	'Mailing list successfully scheduled for creation.' => 'Mailinglist succesvol ingepland om aan te maken.',
+	'Mailing list successfully scheduled for deletion.' => 'Mailinglist succesvol ingepland om te verwijderen.',
+	'Client / Email / Mailman' => 'Klant / E-mail / Mailman',
+	'Mailing Lists' => 'Mailinglists',
+	'Mailing List' => 'Mailinglist',
+	'Add mailing list' => 'Mailinglist toevoegen',
+	'List name' => 'Naam van lijst',
+	'List URL' => 'URL van lijst',
+	'Status' => 'Status',
+	'Actions' => 'Acties',
+	'Admin email' => 'E-mail van beheerder',
+	'Password' => 'Wachtwoord',
+	'Password confirmation' => 'Wachtwoordbevestiging',
+	'Url' => 'URL',
+	'Please, confirm the deletion of the %s mailing list.' => 'Gelieve het verwijderen van de mailinglist %s te bevestigen.',
+	'Save' => 'Opslaan',
+	'Cancel' => 'Annuleren',
+	'This mail account is already used for mailing list.' => 'Dit e-mailadres is reeds in gebruik voor een mailinglist.'
+);
