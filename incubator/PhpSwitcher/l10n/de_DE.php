64137eaa2af4f5f589cfc638a94f95b2e949977c
gOOvER <info@goover.de>
2015-04-10 14:48:12 +0200

    Added German Translation

---
+++ incubator/PhpSwitcher/l10n/de_DE.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * i-MSCP PhpSwitcher plugin
+ * Copyright (C) 2014-2015 Laurent Declercq <l.declercq@nuxwin.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @translator Torsten Widmann (gOOvER) <info@goover.de>
+ */
+
+return array(
+	'This plugin require the apache fcgid i-MSCP server implementation.' => 'DIeses Plugin benötigt die Apache fcgid i-MSCP Server Konfiguration.',
+	'PHP Switcher' => 'PHP Switcher',
+	'PHP version %s has not been found.' => 'PHP Version %s wurde nicht gefunden.',
+	'An unexpected error occurred: %s' => 'Ein unerwarteter Fehler ist aufgetreten: %s',
+	'Bad request.' => 'Ungültige Anforderung.',
+	'All fields are required.' => 'Alle Felder werden benötigt.',
+	'PHP version %s already exists. This is the default PHP version.' => 'PHP Version %s ist bereits vorhanden. Diese ist die Standard PHP Version.',
+	'PHP version %s successfully added.' => 'PHP Version %s erfolgreich hinzugefügt.',
+	'PHP version %s already exists.' => 'PHP Version %s ist bereits vorhanden.',
+	'PHP version %s successfully scheduled for update.' => 'PHP Version %s erfolgreich zur Änderung vorgemerkt.',
+	'PHP version %s successfully scheduled for deletion.' => 'PHP Version %s erfolgreich zur Löschung vorgemerkt.',
+	'Edit this PHP version' => 'Diese PHP Version bearbeiten',
+	'Delete this PHP version' => 'Diese PHP Version löschen',
+	'Admin / Settings / PHP Switcher' => 'Admin / Einstellungen / PHP Switcher',
+	'PHP version' => 'PHP Version',
+	'PHP binary' => 'PHP Binary',
+	'Status' => 'Status',
+	'Actions' => 'Aktionen',
+	'PHP binary path' => 'PHP Binary Pfad',
+	'Processing...' => 'Wird bearbeitet...',
+	'Add new PHP version' => 'Neue PHP Version hinzufügen',
+	'Request Timeout: The server took too long to send the data.' => 'Request Timeout: Der Server brauchte zu lange, um die Daten zu senden.',
+	'An unexpected error occurred.' => 'Ein unerwarteter Fehler ist aufgetreten.',
+	'Unknown Action' => 'Unbekannte Aktion',
+	'New PHP version' => 'Neue PHP Version',
+	'Edit %%s version' => 'Bearbeite %%s Version',
+	'Save' => 'Speichern',
+	'Cancel' => 'Abbruch',
+	'Are you sure you want to delete this PHP version?' => 'Sind Sie sicher, das Sie diese PHP Version löschen wollen?',
+	'PHP version successfully scheduled for update. Please be patient.' => 'PHP Version erfolgreich zum Ändern vorgemerkt. Bitte Warten...',
+	'Client / Settings / PHP Switcher' => 'Kunde / Einstellungen / PHP Switcher',
+	'Please choose the PHP version you want use below.' => 'Bitte wählen Sie die PHP Version, welche Sie nutzen wollen.',
+	'Version' => 'Version',
+	'Update' => 'Ändern'
+);
