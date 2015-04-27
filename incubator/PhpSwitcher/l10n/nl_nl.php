bff101c353c5fe0ab6933f18a3d589a5c95fe23b
Thom <thom@heemstra.us>
2015-04-27 14:47:40 +0200

    PHP Switcher: Dutch translation

---
+++ incubator/PhpSwitcher/l10n/nl_nl.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * i-MSCP PhpSwitcher plugin
+ * Copyright (C) 2014-2015 Laurent Declercq <l.declercq@nuxwin.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @translator Thom Heemstra (theemstra) <thom@heemstra.us>
+ */
+
+return array(
+	'This plugin require the apache fcgid i-MSCP server implementation.' => 'Voor deze plugin is de Apache fcgid serverimplementatie vereist.',
+	'PHP Switcher' => 'PHP Switcher',
+	'PHP version %s has not been found.' => 'PHP versie %s is niet gevonden.',
+	'An unexpected error occurred: %s' => 'Er is een onbekende fout opgetreden: %s',
+	'Bad request.' => 'Slecht verzoek.',
+	'All fields are required.' => 'Alle bestanden zijn vereist.',
+	'PHP version %s already exists. This is the default PHP version.' => 'PHP versie %s bestaat reeds. Dit is de standaard PHP-versie.',
+	'PHP version %s successfully added.' => 'PHP versie %s is succesvol toegevoegd.',
+	'PHP version %s already exists.' => 'PHP versie %s bestaat reeds.',
+	'PHP version %s successfully scheduled for update.' => 'PHP versie %s is succesvol ingepland om bijgewerkt te worden.',
+	'PHP version %s successfully scheduled for deletion.' => 'PHP versie %s is succesvol ingepland om verwijderd te worden.',
+	'Edit this PHP version' => 'PHP-versie aanpassen',
+	'Delete this PHP version' => 'PHP-versie verwijderen',
+	'Admin / Settings / PHP Switcher' => 'Beheerder / Instellingen / PHP Switcher',
+	'PHP version' => 'PHP versie',
+	'PHP binary' => 'PHP binary',
+	'Status' => 'Status',
+	'Actions' => 'Acties',
+	'PHP binary path' => 'PHP binary pad',
+	'Processing...' => 'Verwerken...',
+	'Add new PHP version' => 'Voeg nieuwe PHP-versie toe',
+	'Request Timeout: The server took too long to send the data.' => 'Verzoek timeout: De server deed er te lang over om de informatie te versturen.',
+	'An unexpected error occurred.' => 'Er is een onbekende fout opgetreden.',
+	'Unknown Action' => 'Onbekende actie',
+	'New PHP version' => 'Nieuwe PHP-versie',
+	'Edit %%s version' => 'PHP-versie %%s aanpassen',
+	'Save' => 'Opslaan',
+	'Cancel' => 'Annuleren',
+	'Are you sure you want to delete this PHP version?' => 'Weet u zeker dat u deze PHP-versie wilt verwijderen?',
+	'PHP version successfully scheduled for update. Please be patient.' => 'PHP-versie succesvol ingepland om bijgewerkt te worden, even geduld a.u.b.',
+	'Client / Settings / PHP Switcher' => 'Klant / Instellingen / PHP Switcher',
+	'Please choose the PHP version you want use below.' => 'Kies een PHP-versie om te gebruiken.',
+	'Version' => 'Versie',
+	'Update' => 'Update'
+);
