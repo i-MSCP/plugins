249a906b27c5dc05c7873e4860ba4cc5e6de17d3
Laurent Declercq <l.declercq@nuxwin.com>
2015-05-02 01:07:54 +0200

    PhpSwitcher: Added: Chinese (Taiwan) translation file - Thank to Akong Wu PhpSwitcher: Added: Dutch (Netherlands ) translation file - Thank to Thom Heemstra PhpSwitcher: Added: English (United Kingdom) translation file PhpSwitcher: Added: French (France) translation file PhpSwitcher: Added: German (Germany) translation file - Thank to Torsten Widmann PhpSwitcher: Added: Swedish (Sweden) translation file - Thank to Thom Heemstra PhpSwitcher: Small fixes

---
+++ incubator/PhpSwitcher/l10n/sv_SE.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * i-MSCP PhpSwitcher plugin
+ * Copyright (C) 2014-2015 Laurent Declercq <l.declercq@nuxwin.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @translator W.Wichelmann (Cool) <undisclosed>
+ */
+
+return array(
+	'This plugin require the apache fcgid i-MSCP server implementation.' => 'Detta plugin kräver apache fcgid i-MSCP server.',
+	'PHP Switcher' => 'PHP Switch',
+	'PHP version %s has not been found.' => 'PHP version %s har inte hittats.',
+	'An unexpected error occurred: %s' => 'Ett oväntat fel inträffade: %s',
+	'Bad request.' => 'Dålig förfrågan.',
+	'All fields are required.' => 'Alla fält är obligatoriska.',
+	'PHP version %s already exists. This is the default PHP version.' => 'PHP version %s finns redan. Detta är den standard PHP-version.',
+	'PHP version %s successfully added.' => 'PHP version %s har lagts till.',
+	'PHP version %s already exists.' => 'PHP version %s finns redan.',
+	'PHP version %s successfully scheduled for update.' => 'PHP version %s planerad till uppdatering.',
+	'PHP version %s successfully scheduled for deletion.' => 'PHP version %s planerad till radering.',
+	'Edit this PHP version' => 'Redigera denna PHP-version',
+	'Delete this PHP version' => 'Radera denna PHP-version',
+	'Admin / Settings / PHP Switcher' => 'Admin / Inställningar / PHP Switcher',
+	'PHP version' => 'PHP version',
+	'PHP binary' => 'PHP binary',
+	'Status' => 'Status',
+	'Actions' => 'Åtgärder',
+	'PHP binary path' => 'PHP binary sökväg',
+	'Processing...' => 'Bearbetning...',
+	'Add new PHP version' => 'Lägg till ny PHP-version',
+	'Request Timeout: The server took too long to send the data.' => 'Request Timeout: Servern tog för lång tid att skicka data.',
+	'An unexpected error occurred.' => 'Ett oväntat fel inträffade.',
+	'Unknown Action' => 'Okänd Åtgärd ',
+	'New PHP version' => 'Ny PHP version',
+	'Edit %%s version' => 'Redigera %%s version',
+	'Save' => 'Spara',
+	'Cancel' => 'Annullera',
+	'Are you sure you want to delete this PHP version?' => 'Är du säker på att du vill radera denna PHP-version?',
+	'PHP version successfully scheduled for update. Please be patient.' => 'PHP version planerad till uppdatering. Var tålmodig.',
+	'Client / Settings / PHP Switcher' => 'Kund / Inställningar / PHP Switcher',
+	'Please choose the PHP version you want use below.' => 'Välj PHP version som du vill använda nedan.',
+	'Version' => 'Version',
+	'Update' => 'Updatera'
+);
---
+++ incubator/PhpSwitcher/l10n/zh_TW.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * i-MSCP PhpSwitcher plugin
+ * Copyright (C) 2014-2015 Laurent Declercq <l.declercq@nuxwin.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @translator Akong Wu <akong@aspa.idv.tw>
+ */
+
+return array(
+	'This plugin require the apache fcgid i-MSCP server implementation.' => '�o�ӥ~�������b i-MSCP �W�ϥ� apache fcgid �B�@�C',
+	'PHP Switcher' => 'PHP ��������',
+	'PHP version %s has not been found.' => 'PHP ���� %s �䤣��',
+	'An unexpected error occurred: %s' => '�o�͵L�k�w������~: %s',
+	'Bad request.' => '���n����~�C',
+	'All fields are required.' => '�Ҧ�����쳣�O�������C',
+	'PHP version %s already exists. This is the default PHP version.' => 'PHP ���� %s �w�s�b�C�o�O�w�]�� PHP �����C',
+	'PHP version %s successfully added.' => 'PHP ���� %s �w���\�s�W�F�C',
+	'PHP version %s already exists.' => 'PHP ���� %s �w�s�b�C',
+	'PHP version %s successfully scheduled for update.' => '��s PHP ���� %s �w���\�Ƶ{�F�C',
+	'PHP version %s successfully scheduled for deletion.' => '�R�� PHP ���� %s �w���\�Ƶ{�F�C',
+	'Edit this PHP version' => '�s��o�� PHP ����',
+	'Delete this PHP version' => '�R���o�� PHP ����',
+	'Admin / Settings / PHP Switcher' => '�޲z / �]�w / PHP ����',
+	'PHP version' => 'PHP ����',
+	'PHP binary' => 'PHP ������',
+	'Status' => '���A',
+	'Actions' => '�ʧ@',
+	'PHP binary path' => 'PHP �����ɸ��|',
+	'Processing...' => '���椤...',
+	'Add new PHP version' => '�s�W PHP ����',
+	'Request Timeout: The server took too long to send the data.' => '�ݨD�O��: ����A���ǰe��ƤӤ[�F�C',
+	'An unexpected error occurred.' => '�o�͵L�k�w������~�C',
+	'Unknown Action' => '�����ʧ@',
+	'New PHP version' => '�s�� PHP ����',
+	'Edit %%s version' => '�s�� %%s ����',
+	'Save' => '�x�s',
+	'Cancel' => ' ���',
+	'Are you sure you want to delete this PHP version?' => '�z�T�w�n�R���o�� PHP ������?',
+	'PHP version successfully scheduled for update. Please be patient.' => 'PHP �����w���\�Ƶ{��s�F�C�Э@�ߵ��ԡC',
+	'Client / Settings / PHP Switcher' => '�Ȥ� / �]�w / PHP ����',
+	'Please choose the PHP version you want use below.' => '�п�ܱz�Q�ϥΪ� PHP �����C',
+	'Version' => '����',
+	'Update' => '��s'
+);
