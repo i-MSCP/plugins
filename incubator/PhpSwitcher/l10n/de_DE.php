63a7f0810aa5f193d590297399dada5317d8ec16
Laurent Declercq <l.declercq@nuxwin.com>
2015-05-02 03:02:00 +0200

    PhpSwitcher: Added language tag

--- incubator/PhpSwitcher/l10n/de_DE.php
+++ incubator/PhpSwitcher/l10n/de_DE.php
@@ -17,6 +17,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
+ * @language German (Germany)
  * @translator Torsten Widmann (gOOvER) <info@goover.de>
  */
 
--- incubator/PhpSwitcher/l10n/nl_nl.php
+++ incubator/PhpSwitcher/l10n/nl_nl.php
@@ -17,6 +17,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
+ * @language Dutch (Netherlands)
  * @translator Thom Heemstra (theemstra) <thom@heemstra.us>
  */
 
