4b474d22a1f850cc5e4473a04cb1e2622c63d040
Laurent Declercq <l.declercq@nuxwin.com>
2015-05-04 19:44:25 +0200

    PhpSwitcher: Added: Russian (Russia) translation file - Thank to Vitaliy Zhukov

---
+++ incubator/PhpSwitcher/l10n/ru_RU.php
@@ -0,0 +1,68 @@
+﻿<?php
+/**
+ * i-MSCP PhpSwitcher plugin
+ * Copyright (C) 2014-2015 Laurent Declercq <l.declercq@nuxwin.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @language Russian (Russia)
+ * @translator Vitaliy Zhukov (DragonZX) <undisclosed>
+ */
+
+return array(
+	'This plugin require the apache fcgid i-MSCP server implementation.' => 'Этому плагину требуется apache fcgid режим i-MSCP сервера.',
+	'PHP Switcher' => 'Версии PHP',
+	'PHP version %s has not been found.' => 'PHP версии %s не был найден.',
+	'An unexpected error occurred: %s' => 'Произошла неизвестная ошибка: %s',
+	'Bad request.' => 'Неверный запрос.',
+	'All fields are required.' => 'Требуется заполнить все поля.',
+	'PHP version %s already exists.' => 'PHP версии %s уже существует.',
+	'PHP version %s has been scheduled for addition.' => 'PHP версии %s успешно добавлен.',
+	'PHP version %s already exists. This is the default PHP version.' => 'PHP версии %s уже существует. Это версия PHP по умолчанию.',
+	'PHP version %s successfully scheduled for update.' => 'PHP версии %s успешно помечен для добавления.',
+	'PHP version %s successfully updated.' => 'PHP version %s successfully updated.',
+	'Nothing has been changed.' => 'Nothing has been changed.',
+	'PHP version %s successfully scheduled for deletion.' => 'PHP version %s успешно помечен для удаления.',
+	'PHP info files were successfully scheduled for re-generation.' => 'PHP info files were successfully scheduled for re-generation.',
+	'Edit this PHP version' => 'Изменить эту версию PHP',
+	'Delete this PHP version' => 'Удалить эту версию PHP',
+	'Save' => 'Сохранить',
+	'Cancel' => 'Отмена',
+	'New PHP version' => 'Новая версия PHP',
+	'Edit %%s version' => 'Изменить %%s версию',
+	'Are you sure you want to delete this PHP version?' => 'Вы действительно хотите удалить эту версию PHP?',
+	'Unknown Action' => 'Неизвестное действие',
+	'Request Timeout: The server took too long to send the data.' => 'Прерывание запроса:Сервер слишком долго обрабатывает данные.',
+	'An unexpected error occurred.' => 'Произошла неожиданная ошибка.',
+	'Are you sure you want to re-generate all PHP info files?' => 'Are you sure you want to re-generate all PHP info files?',
+	'Admin / Settings / PHP Switcher' => 'Администратор / Настройки / Версии PHP',
+	'PHP version' => 'Версия PHP',
+	'PHP binary' => 'PHP файл',
+	'Status' => 'Статус',
+	'Actions' => 'Действия',
+	'PHP binary path' => 'Путь к файлу PHP',
+	'Processing...' => 'Выполнение...',
+	'Add new PHP version' => 'Добавить новую версию PHP',
+	'Re-generate phpinfo' => 'Re-generate phpinfo',
+	'Re-generate PHP info files.' => 'Re-generate PHP info files.',
+	'PHP version successfully scheduled for update. Please be patient.' => 'Версия PHP успешно помечена для обновления. Пожалуйста подождите.',
+	'Close' => 'Close',
+	'Client / Settings / PHP Switcher' => 'Клиент / Настройки / Версии PHP',
+	'For each domain listed below, you can choose the PHP version you want use by selecting it.<br>Be aware that domains which are redirected on a specific URL are discarded.' => 'For each domain listed below, you can choose the PHP version you want use by selecting it.<br>Be aware that domains which are redirected on a specific URL are discarded.',
+	'Domain name' => 'Domain name',
+	'PHP version info' => 'PHP version info',
+	'Show info' => 'Show info',
+	'Domain status' => 'Domain status',
+);
