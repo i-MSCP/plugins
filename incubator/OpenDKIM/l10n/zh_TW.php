ae3e577dada5161ed31644a1788917c79104f2cd
akong77 <akong@aspa.idv.tw>
2015-06-17 21:45:15 +0800

    Create zh_TW.php

---
+++ incubator/OpenDKIM/l10n/zh_TW.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * i-MSCP OpenDKIM plugin
+ * Copyright (C) 2013-2015 Laurent Declercq <l.declercq@nuxwin.com>
+ * Copyright (C) 2013-2015 Rene Schuster <mail@reneschuster.de>
+ * Copyright (C) 2013-2015 Sascha Bay <info@space2place.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @translator  Sascha Bay <info@space2place.de>
+ */
+return array(
+	'OpenDKIM support scheduled for activation. This can take few seconds.' => '已排程啟用 OpenDKIM 的功能。這需要幾秒鐘。',
+	'OpenDKIM support scheduled for deactivation. This can take few seconds.' => '已排程停用 OpenDKIM 的功能。這需要幾秒鐘。',
+	'Generation in progress.' => '正在產生中。',
+	'n/a' => '沒有',
+	'OpenDKIM entries for customer: %s' => '使用 OpenDKIM 的客戶: %s',
+	'Deactivate OpenDKIM' => '停用 OpenDKIM',
+	'No customer with OpenDKIM support has been found.' => '沒有找到使用 OpenDKIM 的客戶。',
+	'Customers / OpenDKIM' => '客戶 / OpenDKIM',
+	'Select a customer' => '選擇客戶',
+	'Activate OpenDKIM for this customer' => '啟動 OpenDKIM 給這個客戶',
+	'Domain Name' => '網域名稱',
+	'OpenDKIM domain key' => 'OpenDKIM 網域金鑰',
+	'Status' => '狀態',
+	'Are you sure you want to deactivate OpenDKIM for this customer?' => '您確定要停用這個客戶的 OpenDKIM 功能嗎?',
+	'No domain with OpenDKIM support has been found.' => '沒有找到使用 OpenDKIM 的網域。',
+	'Domain' => '網域',
+	'Name' => '名稱',
+	'OpenDKIM' => 'OpenDKIM'
+);
