eb63b66379a1b33de68655401a946aa41c011673
Ninos Ego <me@ninosego.de>
2015-07-13 17:20:42 +0200

    RoundcubePlugins: Preparation for new release

--- incubator/RoundcubePlugins/info.php
+++ incubator/RoundcubePlugins/info.php
@@ -25,9 +25,9 @@ return array(
 		'Sascha Bay'
 	),
 	'email' => 'team@i-mscp.net',
-	'version' => '1.2.1',
+	'version' => '1.2.2',
 	'require_api' => '1.0.0',
-	'date' => '2015-07-12',
+	'date' => '2015-07-13',
 	'name' => 'RoundcubePlugins',
 	'desc' => 'Plugin allows to use Roundcube Plugins with i-MSCP',
 	'url' => 'http://wiki.i-mscp.net/doku.php?id=plugins:roundcubeplugins'
