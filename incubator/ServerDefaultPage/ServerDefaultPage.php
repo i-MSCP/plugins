05fd7becee2c141ec205148eabf7efe5d426619a
Ninos Ego <me@ninosego.de>
2016-01-17 20:44:14 +0100

    ServerDefaultPage: Updated Copyright

--- incubator/ServerDefaultPage/ServerDefaultPage.php
+++ incubator/ServerDefaultPage/ServerDefaultPage.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * i-MSCP ServerDefaultPage plugin
- * Copyright (C) 2014-2015 by Ninos Ego <me@ninosego.de>
+ * Copyright (C) 2014-2016 by Ninos Ego <me@ninosego.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
