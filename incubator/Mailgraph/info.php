36efc2c98af95c768a7fa905587da49d39c4e775
Ninos Ego <me@ninosego.de>
2016-05-24 22:42:33 +0200

    Mailgraph: Increase version to 1.1.0

--- incubator/Mailgraph/info.php
+++ incubator/Mailgraph/info.php
@@ -22,7 +22,7 @@
 return array(
     'author' => 'Sascha Bay',
     'email' => 'info@space2place.de',
-    'version' => '1.0.1',
+    'version' => '1.1.0',
     'require_api' => '1.0.4',
     'date' => '2016-05-24',
     'name' => 'Mailgraph',
