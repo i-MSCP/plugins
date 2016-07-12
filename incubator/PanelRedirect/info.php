e99449639eda908cde8ecc9c882b8867993db045
Ninos Ego <me@ninosego.de>
2016-07-13 00:36:40 +0200

    PanelRedirect: Decrease api version (increase was not needed)

--- incubator/PanelRedirect/info.php
+++ incubator/PanelRedirect/info.php
@@ -22,7 +22,7 @@ return array(
     'author'      => 'Ninos Ego',
     'email'       => 'me@ninosego.de',
     'version'     => '1.1.3',
-    'require_api' => '1.0.5',
+    'require_api' => '1.0.4',
     'date'        => '2016-07-13',
     'name'        => 'PanelRedirect',
     'desc'        => 'Plugin that provides access to i-MSCP panel (and tools) through standard https ports (80, 443)',
