0012af4fe00af9729e331479cdb9e3a0eae6a07e
theqkash <qkash@o2.pl>
2016-09-13 21:04:48 +0200

    Created pl_PL translation

---
+++ incubator/OpenDKIM/l10n/pl_PL.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * i-MSCP OpenDKIM plugin
+ * Copyright (C) 2013-2016 Laurent Declercq <l.declercq@nuxwin.com>
+ * Copyright (C) 2013-2016 Rene Schuster <mail@reneschuster.de>
+ * Copyright (C) 2013-2016 Sascha Bay <info@space2place.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @translator  Łukasz "theqkash" Nowak <theqkash@theqkash.pl>
+ */
+
+return array(
+    'OpenDKIM support scheduled for activation. This can take few seconds.' => 'Wsparcie dla OpenDKIM zaplanowane do aktywacji. Może to potrwać kilka sekund.',
+    'OpenDKIM support scheduled for deactivation. This can take few seconds.' => 'Wsparcie dla OpenDKIM zaplanowane do deaktywacji. Może to potrwać kilka sekund.',
+    'Generation in progress.' => 'Generowanie w trakcie.',
+    'n/a' => 'n/a',
+    'OpenDKIM entries for customer: %s' => 'Wpisy OpenDKIM dla klienta: %s',
+    'Deactivate OpenDKIM' => 'Deactiwuj OpenDKIM',
+    'No customer with OpenDKIM support has been found.' => 'Nie znaleziono żadnego klienta z włączonym OpenDKIM.',
+    'Customers / OpenDKIM' => 'Klienci / OpenDKIM',
+    'Select a customer' => 'Wybierz klienta',
+    'Activate OpenDKIM for this customer' => 'Aktywuj OpenDKIM dla tego klienta',
+    'Domain Name' => 'Nazwa domeny',
+    'OpenDKIM domain key' => 'Klucz domeny OpenDKIM',
+    'Status' => 'Status',
+    'Are you sure you want to deactivate OpenDKIM for this customer?' => 'Czy na pewno chcesz deaktywować OpenDKIM dla tego klienta?',
+    'No domain with OpenDKIM support has been found.' => 'Nie znaleziono żadnej domeny z włączonym OpenDKIM.',
+    'Domain' => 'Domena',
+    'Name' => 'Nazwa',
+    'OpenDKIM' => 'OpenDKIM'
+);
